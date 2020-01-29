c0ca25b4a3b99a7bbb23ec9286bd282f1244d711
Suyash Ojha <60437257+SoarixLazy@users.noreply.github.com>
2020-01-29 16:39:03 -0600

    Update WIN_TH0T_SL4YER.bat

--- scripts/WIN_TH0T_SL4YER.bat
+++ scripts/WIN_TH0T_SL4YER.bat
@@ -1,1 +1,11 @@
-Being worked on
+@echo off
+echo Setting the lockout policy
+net accounts /lockoutduration:30
+net accounts /lockoutthreshold:6
+net accounts /lockoutwindow:30
+
+echo Setting pasword policies
+net accounts /minpwlen:8
+net accounts /maxpwage:30
+net accounts /minpwage:10
+net accounts /uniquepw:3
