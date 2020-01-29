0d0563b4e8a56f071878d597d66d290e100c9e04
Suyash Ojha <60437257+SoarixLazy@users.noreply.github.com>
2020-01-29 17:05:42 -0600

    Update WIN_TH0T_SL4YER.bat

--- scripts/WIN_TH0T_SL4YER.bat
+++ scripts/WIN_TH0T_SL4YER.bat
@@ -1,11 +1,14 @@
 @echo off
-echo Setting the lockout policy
+
+Rem this script is still being worked on won't be fully operational until the end of the summer - Suyash
+
 net accounts /lockoutduration:30
 net accounts /lockoutthreshold:6
 net accounts /lockoutwindow:30
 
-echo Setting pasword policies
 net accounts /minpwlen:8
 net accounts /maxpwage:30
 net accounts /minpwage:10
 net accounts /uniquepw:3
+
+Rem This script is owned by C0DE R3D, developed by Suyash Ojha
