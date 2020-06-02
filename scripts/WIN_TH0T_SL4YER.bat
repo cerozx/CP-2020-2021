da6ca2c1a96aa71276cc6163b30d58b4558e27de
Suyash Ojha <60437257+SoarixLazy@users.noreply.github.com>
2020-06-01 22:20:22 -0500

    Update WIN_TH0T_SL4YER.bat

--- scripts/WIN_TH0T_SL4YER.bat
+++ scripts/WIN_TH0T_SL4YER.bat
@@ -1,6 +1,8 @@
 @echo off
 
-Rem this script is still being worked on won't be fully operational until the end of the summer - Suyash
+REM this script is still being worked on won't be fully operational until the end of the summer - Suyash
+
+REM Basic password policies there are one or two of them you gotta do manually
 
 net accounts /lockoutduration:30
 net accounts /lockoutthreshold:6
@@ -11,4 +13,14 @@ net accounts /maxpwage:30
 net accounts /minpwage:10
 net accounts /uniquepw:3
 
-Rem This script is owned by C0DE R3D, developed by Suyash Ojha
+REM Disables guest
+
+net userguest /active:no
+
+REM Enables the firewall
+
+netsh advfirewall set allprofiles state on
+
+REM Disables Telnet I am still working on this one
+
+REM  This script is owned by C0DE R3D, developed by Suyash Ojha
